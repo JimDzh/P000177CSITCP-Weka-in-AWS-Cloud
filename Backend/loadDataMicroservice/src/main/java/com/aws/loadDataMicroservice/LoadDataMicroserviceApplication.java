d81895e39c111760695b5b82ed79f351fb67e6c1
christo-rmit <s3738844@student.rmit.edu.au>
2022-09-17 17:22:43 +1000

    FilterMicroservice and SparseMicroservice

--- Backend/loadDataMicroservice/src/main/java/com/aws/loadDataMicroservice/LoadDataMicroserviceApplication.java
+++ Backend/loadDataMicroservice/src/main/java/com/aws/loadDataMicroservice/LoadDataMicroserviceApplication.java
@@ -15,21 +15,21 @@ public class LoadDataMicroserviceApplication {
 	public static void main(String[] args) {
 
 		// CONVERTING CSV TO ARFF
-		CSVLoader loader = new CSVLoader();
-		try{
-			loader.setSource(new File("./Datasets/testCsv.csv"));
-			Instances data = loader.getDataSet();
-			ArffSaver saver = new ArffSaver();
-			saver.setInstances(data);
-			saver.setFile(new File("./Datasets/resultArff.arff"));
-			saver.writeBatch();
-		} catch (Exception e) {
-			System.out.println("ATTEMPT UNSUCCESSFUL");
-		}
+//		CSVLoader loader = new CSVLoader();
+//		try{
+//			loader.setSource(new File("./Datasets/testCsv.csv"));
+//			Instances data = loader.getDataSet();
+//			ArffSaver saver = new ArffSaver();
+//			saver.setInstances(data);
+//			saver.setFile(new File("./Datasets/resultArff.arff"));
+//			saver.writeBatch();
+//		} catch (Exception e) {
+//			System.out.println("ATTEMPT UNSUCCESSFUL");
+//		}
 
 		// LOADING A DATASET
 		try{
-			Instances dataset = new Instances(new BufferedReader(new FileReader("./Datasets/testArff.arff")));
+			Instances dataset = new Instances(new BufferedReader(new FileReader("P000177CSITCP-Weka-in-AWS-Cloud/Datasets/testArff_sparse.arff")));
 			System.out.println(dataset.toSummaryString());
 		} catch (Exception e) {
 			System.out.println("ATTEMPT UNSUCCESSFUL");
